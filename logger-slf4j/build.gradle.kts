65dbe0c5faee75d3239b64dd5a45237fc10f8315
Scarlet Pan <scarletpan@qq.com>
2025-11-16 21:00:27 +0800

    feat logger-slf4j v1.0.0： 1.工程配置。 2.模块发布配置。

--- logger-slf4j/build.gradle.kts
+++ logger-slf4j/build.gradle.kts
@@ -1,19 +1,71 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.maven.publish)
+    signing
 }
+
+group = "io.github.scarlet-pan"
+version = "1.0.0-SNAPSHOT"
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
+
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_1_8
     }
 }
+
 dependencies {
-    implementation(project(":logger"))
+    implementation(libs.logger)
     implementation(libs.slf4j.api)
     testImplementation(libs.kotlin.test)
     testImplementation(libs.slf4j.simple)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "logger-slf4j", version.toString())
+
+    pom {
+        name = "Logger-SLF4J"
+        description = "A Kotlin Multiplatform logging library."
+        inceptionYear = "2025"
+        url = "https://github.com/scarlet-pan/logger"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+        developers {
+            developer {
+                id = "Scarlet-Pan"
+                name = "Scarlet Pan"
+                email = "scarletpan@qq.com"
+            }
+        }
+        scm {
+            url = "https://github.com/scarlet-pan/logger"
+            connection = "scm:git:https://github.com/scarlet-pan/logger.git"
+            developerConnection = "scm:git:ssh://git@github.com/scarlet-pan/logger.git"
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        providers.gradleProperty("signingInMemoryKeyId").orNull,
+        providers.gradleProperty("signingInMemoryKey").orNull,
+        providers.gradleProperty("signingInMemoryKeyPassword").orNull
+    )
+    sign(publishing.publications)
 }
