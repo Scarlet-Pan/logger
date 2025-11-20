11b6fc73aeb6749c7aa215ebc089653c0aa508a0
Scarlet Pan <scarletpan@qq.com>
2025-11-20 20:31:30 +0800

    feature logger v1.1.0： 1.AbsLogger，抽象基类，便于实现； 2.部分类改为集成AbsLogger，减少源码量。

--- logger/src/jsMain/kotlin/dev/scarlet/logger/PlatformLogger.js.kt
+++ logger/src/jsMain/kotlin/dev/scarlet/logger/PlatformLogger.js.kt
@@ -7,33 +7,17 @@ import dev.scarlet.logger.Logger.Level.INFO
 import dev.scarlet.logger.Logger.Level.WARN
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-internal actual object PlatformLogger : Logger {
+internal actual object PlatformLogger : AbsLogger() {
 
     actual override fun toString(): String = "ConsoleLogger"
 
-    actual override fun d(tag: String, msg: String, tr: Throwable?) {
-        log(DEBUG, tag, msg, tr)
-    }
-
-    actual override fun i(tag: String, msg: String, tr: Throwable?) {
-        log(INFO, tag, msg, tr)
-    }
-
-    actual override fun w(tag: String, msg: String, tr: Throwable?) {
-        log(WARN, tag, msg, tr)
-    }
-
-    actual override fun e(tag: String, msg: String, tr: Throwable?) {
-        log(ERROR, tag, msg, tr)
-    }
-
-    private fun log(level: Level, tag: String, message: String, throwable: Throwable?) {
-        val fullMessage = "[$tag] $message"
+    override fun log(level: Level, tag: String, msg: String, tr: Throwable?) {
+        val fullMessage = "[$tag] $msg"
         when (level) {
-            DEBUG -> console.log(fullMessage, throwable?.toJsError())
-            INFO -> console.info(fullMessage, throwable?.toJsError())
-            WARN -> console.warn(fullMessage, throwable?.toJsError())
-            ERROR -> console.error(fullMessage, throwable?.toJsError())
+            DEBUG -> console.log(fullMessage, tr?.toJsError())
+            INFO -> console.info(fullMessage, tr?.toJsError())
+            WARN -> console.warn(fullMessage, tr?.toJsError())
+            ERROR -> console.error(fullMessage, tr?.toJsError())
         }
     }
 
