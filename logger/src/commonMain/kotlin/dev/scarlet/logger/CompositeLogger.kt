12c79371dfdc018b8e7c9836eeebf980c25bcc3b
Scarlet Pan <scarletpan@qq.com>
2025-11-22 11:48:48 +0800

    feature logger v1.0.1： 1.修复Logger.plus编译打包时找不到的问题。

--- logger/src/commonMain/kotlin/dev/scarlet/logger/CompositeLogger.kt
+++ logger/src/commonMain/kotlin/dev/scarlet/logger/CompositeLogger.kt
@@ -1,10 +1,4 @@
-@file:JvmName("Loggers")
-@file:JvmMultifileClass
-
 package dev.scarlet.logger
-
-import kotlin.jvm.JvmMultifileClass
-import kotlin.jvm.JvmName
 
 /**
  * A composite logger that combines two [Logger] instances.
@@ -49,22 +43,3 @@ internal class CompositeLogger(
     }
 }
 
-/**
- * Combines this logger with another [Logger], returning a new composite logger.
- *
- * The returned [Logger] dispatches every log call **sequentially** to both original loggers:
- * first to `this`, then to [other].
- *
- * Chaining is supported. For example:
- * ```kotlin
- * Logger.default = Logger.SYSTEM + FileLogger + NetworkLogger
- * ```
- *
- * The resulting logger fully adheres to the [Logger] interface and can be further composed
- * or assigned to [Logger.default].
- *
- * @param other The logger to combine with this one.
- * @return A new [Logger] instance that sends each log message first to `this`, then to [other].
- */
-@JvmName("combine")
-operator fun Logger.plus(other: Logger): Logger = CompositeLogger(this, other)
